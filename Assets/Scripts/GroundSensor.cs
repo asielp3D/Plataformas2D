765003e8e222d5b3ff1630db439a203b1753251c
asielp3D <asielmasespacio@gmail.com>
2023-09-20 11:05:14 +0200

    SALTO

---
+++ Assets/Scripts/GroundSensor.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSensor : MonoBehaviour
+{
+    public bool _isGrounded;
+   
+   void OntriggerEnter2D(Collider2D other)
+   {
+        if(other.gameObject.layer == 6)
+        {
+           _isGrounded = true; 
+        }
+        
+   }
+
+   void OntriggerExit2D(Collider2D other)
+   {
+         if(other.gameObject.layer == 6)
+        {
+           _isGrounded = false; 
+        }
+   }
+
+   void OntriggerStay2D(Collider2D other)
+   {
+         if(other.gameObject.layer == 6)
+        {
+           _isGrounded = true; 
+        }
+   }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,25 +5,47 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]private float _playerSpeed = 5;
+    [SerializeField]private float _jumpForce = 5;
     private float _playerInputHorizontal;
     private float _playerInputVertical;
+    private Rigidbody2D _rBody2D;
+    private GroundSensor _sensor;
     // Start is called before the first frame update
     void Start()
     {
-       
+       _rBody2D = GetComponent <Rigidbody2D>();
+       _sensor = GetComponentInChildren<GroundSensor>();
     }
 
     // Update is called once per frame
     void Update()
     {
        PlayerMovement(); 
+
+       if(Input.GetButtonDown("Jump") && _sensor._isGrounded)
+       {
+           Jump();
+       }
+    }
+
+    void FixedUpdate()
+    {
+        //_rBody2D.AddForce(new Vector2(_playerInputHorizontal * _playerSpeed, 0), ForceMode2D.Impulse);
+
+        _rBody2D.velocity = new Vector2(_playerInputHorizontal * _playerSpeed, _rBody2D.velocity.y);
     }
 
     void PlayerMovement()
     {
         _playerInputHorizontal = Input.GetAxis("Horizontal");
-        _playerInputVertical = Input.GetAxis("Vertical");
+        /*_playerInputVertical = Input.GetAxis("Vertical");
 
-        transform.Translate(new Vector2(_playerInputHorizontal,_playerInputVertical) * _playerSpeed * Time.deltaTime);
+        transform.Translate(new Vector2(_playerInputHorizontal,_playerInputVertical) * _playerSpeed * Time.deltaTime);*/
     }
+
+    void Jump()
+    {
+         _rBody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+    }
+
 }
